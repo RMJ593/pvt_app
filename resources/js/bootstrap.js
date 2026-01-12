b11f8a5c853313c0e567b5ff318b93edcbbcd1d8
RIYA MARY JOSE <riyamary593@gmail.com>
2026-01-12 15:17:42 +0530

    Fix API URLs to use current domain

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,20 +1,15 @@
 import axios from 'axios';
 
-// TEMPORARY FIX: Hardcode to use current domain
-// This ensures it always calls the same backend it's deployed on
-axios.defaults.baseURL = `${window.location.origin}/api`;
+// Use current domain only - NO /api suffix here
+axios.defaults.baseURL = window.location.origin;
 
 console.log('🔍 Axios Base URL:', axios.defaults.baseURL);
 
-// Enable credentials
 axios.defaults.withCredentials = true;
-
-// Set default headers
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
-// Add request interceptor to include auth token
 axios.interceptors.request.use(
     (config) => {
         console.log('📤 Request:', config.method?.toUpperCase(), config.url);
@@ -29,7 +24,6 @@ axios.interceptors.request.use(
     }
 );
 
-// Add response interceptor for 401 errors
 axios.interceptors.response.use(
     (response) => response,
     (error) => {
