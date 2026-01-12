8ce9f42a727cc2366b21e3269c9579edc0391881
RIYA MARY JOSE <riyamary593@gmail.com>
2026-01-12 14:26:10 +0530

    Add global axios configuration

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,19 @@
+// resources/js/app.jsx (or main entry point)
 import './bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import AdminApp from './AdminApp';
 import CustomerApp from './customer/CustomerApp';
+
+// ⚠️ GLOBAL AXIOS CONFIGURATION - Must be BEFORE any components load
+axios.defaults.baseURL = `${window.location.origin}/api`;
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['Accept'] = 'application/json';
+
+console.log('🌍 Global Axios Base URL:', axios.defaults.baseURL);
 
 function App() {
     const path = window.location.pathname;
